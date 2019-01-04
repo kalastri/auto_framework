30b26e0c11b656c3c9453ea54b496b78c2be09a2
kalastri <narasimhakalastri@gmail.com>
2019-01-04 07:46:16 +0530

    initial setup for frame work

---
+++ src/test/java/com/automation/facebook/FBLogin.java
@@ -0,0 +1,69 @@
+package com.automation.facebook;
+import org.testng.Reporter;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.cts.objectrepository.ObjectRepository;
+import com.cts.panels.vbr.Panel;
+
+public class FBLogin extends Panel{
+	
+	@Test
+	@Parameters({"strDatalocation","strDatasheet","Browser"})
+	public void TestSetup(String strDatalocation,String strDatasheet,String Browser){
+		
+		FBLogin fl=new FBLogin(); 
+		try{
+			fl.setup(strDatalocation, strDatasheet, Browser);
+			fl.test();
+			driver.quit();
+			Reporter.log("Test Script pass");
+		}catch(Exception e){
+			e.printStackTrace();
+			Reporter.log(" Test Script fail");
+		}
+}
+	
+	public void test()  throws Exception {
+		
+		driver.get("https://www.facebook.com");
+		
+       textfield.findByXpath(ObjectRepository.fbuser);		
+		
+		data.setActiveSheet("Yahoo");
+		
+		Reporter.log(" facebook user name entered in the text box");
+		
+		textfield.setValue(data.getIterationData("UserName", 1));
+		
+		Thread.sleep(5000);		
+		
+		textfield.findByXpath(ObjectRepository.fbpwd);		
+		
+		textfield.setValue(data.getIterationData("Password", 1));		
+	
+		
+		
+		
+		Reporter.log("Password entered in the text box");		
+		
+		Thread.sleep(3000);
+		
+		button.findByXpath(ObjectRepository.fblog);		
+		
+		button.click();
+		
+		Reporter.log("Clicked on sign in Button");
+		
+		Thread.sleep(4000);
+//		
+//	    button.findByXpath(ObjectRepository.forgotPwd);	    
+//	   
+//	    button.click();
+//	    
+//	    Reporter.log("forgot pwd link is clicked");		
+//		
+		}		
+		
+		}
+
